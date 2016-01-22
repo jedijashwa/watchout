28fbe1e96ca4e21dd6db73f0fe232b6716b2e2ff
Josh Riesenbach <josh.riesenbach@gmail.com>
2016-01-22 16:36:34 -0700

    Made cloud of circles

--- client/watchout.js
+++ client/watchout.js
@@ -1,3 +1,19 @@
-// start slingin' some d3 here.
+// start slingin' some d3 
+var svg = d3.select(".container").append('svg')
+  .attr('width', 800)
+  .attr('height', 400);
 
-jklj;k
+var circles = svg.selectAll('circle').data(Array(1000)).enter()
+  .append('circle').attr('r', 10)
+  .attr('class', 'enemy')
+  .attr('cx',400)
+  .attr('cy',200)
+
+var mobility = function(){
+  circles.transition().duration(1000)
+  .attr('cx',function(d){return Math.random()*800})
+  .attr('cy',function(d){return Math.random()*400})
+  setTimeout(mobility, 100)
+}
+
+mobility();
